e9ece16301d720338fb5eed2d9f641954a22db97
Connorris <norris.connor.j@gmail.com>
2024-01-24 14:46:05 -0700

    some stuff

--- src/main/java/frc/robot/DIOSensor.java
+++ src/main/java/frc/robot/DIOSensor.java
@@ -8,12 +8,13 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+private DigitalInput DIO;
+
 public class DIOSensor {
-
-    public static TalonFX switchTest = new TalonFX(14);
-    //hi help
-
-    public static void sensorTest(double speed, double negSpeed) {
+    public DIOSensor (int port, String type){
+        this.DIO = new DigitalInput(port);
+    }
+    public static void DIO(double speed, double negSpeed) {
 
         if (limitSwitch.get()) {
             switchTest.set(ControlMode.PercentOutput, 0);
