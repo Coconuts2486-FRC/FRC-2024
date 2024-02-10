381d2b3c85702ab204e325b3c294c427663630c8
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-02-10 10:59:59 -0700

    Launcher Pid

--- src/main/java/frc/robot/Launcher.java
+++ src/main/java/frc/robot/Launcher.java
@@ -2,11 +2,12 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Launcher {
     static boolean launcherReady = false;
-
+    static PIDController pivotLauncher = new PIDController(.5, 0.001, 0);
     public static void powerUp(double buttonPress) {
         if (buttonPress >= .15) {
             Map.rightLauncher.set(ControlMode.PercentOutput, 1);
