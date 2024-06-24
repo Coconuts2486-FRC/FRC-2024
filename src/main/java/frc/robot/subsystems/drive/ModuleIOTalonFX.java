113c9b93a26d5d35624bb688abd7eb04354781d9
Connorris <norris.connor.j@gmail.com>
2024-06-24 12:12:35 -0700

    motor offset

--- src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOTalonFX.java
@@ -65,32 +65,32 @@ public class ModuleIOTalonFX implements ModuleIO {
 
   private final boolean isTurnMotorInverted = true;
   private final Rotation2d absoluteEncoderOffset;
-
+  // FL, FR, BL, BR
   public ModuleIOTalonFX(int index) {
     switch (index) {
       case 0:
         driveTalon = new TalonFX(2, "drive");
         turnTalon = new TalonFX(6, "drive");
         cancoder = new CANcoder(10, "drive");
-        absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
+        absoluteEncoderOffset = new Rotation2d(Math.toRadians(14.3)); // MUST BE CALIBRATED
         break;
       case 1:
         driveTalon = new TalonFX(3, "drive");
         turnTalon = new TalonFX(7, "drive");
         cancoder = new CANcoder(11, "drive");
-        absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
+        absoluteEncoderOffset = new Rotation2d(Math.toRadians(46.7)); // MUST BE CALIBRATED
         break;
       case 2:
         driveTalon = new TalonFX(4, "drive");
         turnTalon = new TalonFX(8, "drive");
         cancoder = new CANcoder(12, "drive");
-        absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
+        absoluteEncoderOffset = new Rotation2d(Math.toRadians(5.19)); // MUST BE CALIBRATED
         break;
       case 3:
         driveTalon = new TalonFX(5, "drive");
         turnTalon = new TalonFX(9, "drive");
         cancoder = new CANcoder(13, "drive");
-        absoluteEncoderOffset = new Rotation2d(0.0); // MUST BE CALIBRATED
+        absoluteEncoderOffset = new Rotation2d(Math.toRadians(-106.5)); // MUST BE CALIBRATED
         break;
       default:
         throw new RuntimeException("Invalid module index");
