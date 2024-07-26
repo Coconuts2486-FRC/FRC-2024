4e47d61ec1d451c9ecebbe2e47ba80083d23ca70
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-07-26 12:20:22 -0700

    Revert "What is happening"
    This reverts commit 194fd195881bbc66f56de21d5921cd5191e10836.

---
+++ src/main/java/frc/robot/commands/AutoIntakeCommand.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.intake.IntakeRollers;
+import java.util.function.BooleanSupplier;
+
+public class AutoIntakeCommand extends Command {
+  private final Intake intake;
+  private final IntakeRollers intakeRollers;
+  private final BooleanSupplier lightStop;
+  private final BooleanSupplier intakeStop;
+
+  public AutoIntakeCommand(
+      IntakeRollers intakeRollers,
+      Intake intake,
+      BooleanSupplier lightStop,
+      BooleanSupplier intakeStop) {
+    this.intake = intake;
+    this.lightStop = lightStop;
+    this.intakeRollers = intakeRollers;
+    this.intakeStop = intakeStop;
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    if (lightStop.getAsBoolean()) {
+      intakeRollers.setRollers(0, 0);
+      if (intakeStop.getAsBoolean()) {
+        intake.stop();
+      } else {
+        intake.retract();
+      }
+    } else {
+      intake.setExtendPosition(48.8);
+      intakeRollers.setRollers(.3, 0.35);
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intake.stop();
+  }
+}
