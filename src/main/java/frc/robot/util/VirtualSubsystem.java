bc4e44b7a1cf6ec6cbbcbabf0f6ce9d6c427233e
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2024-08-11 16:03:23 -0700

    First steps at adding PhotonVision in the AK style
    	modified:   src/main/java/frc/robot/Constants.java
    	new file:   src/main/java/frc/robot/FieldConstants.java
    	new file:   src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVision.java
    	new file:   src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionConstants.java
    	new file:   src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionIO.java
    	new file:   src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionIOPhotonVision.java
    	new file:   src/main/java/frc/robot/util/Alert.java
    	new file:   src/main/java/frc/robot/util/VirtualSubsystem.java

---
+++ src/main/java/frc/robot/util/VirtualSubsystem.java
@@ -0,0 +1,29 @@
+// Copyright (c) 2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+// Copyright (c) 2024 FRC 2486
+// http://github.com/Coconuts2486-FRC
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class VirtualSubsystem {
+  private static List<VirtualSubsystem> subsystems = new ArrayList<>();
+
+  public VirtualSubsystem() {
+    subsystems.add(this);
+  }
+
+  public static void periodicAll() {
+    for (VirtualSubsystem subsystem : subsystems) {
+      subsystem.periodic();
+    }
+  }
+
+  public abstract void periodic();
+}
