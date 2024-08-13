4dc940ab3aa1da5ae7196d66c08a10a3a3d87ba6
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2024-08-13 13:07:30 -0700

    Commenting out files we're not using (yet)
    	modified:   src/main/java/frc/robot/FudgeFactors.java
    	modified:   src/main/java/frc/robot/RobotState.java

--- src/main/java/frc/robot/FudgeFactors.java
+++ src/main/java/frc/robot/FudgeFactors.java
@@ -1,48 +1,49 @@
-// Copyright (c) 2024 FRC 6328
-// http://github.com/Mechanical-Advantage
-// Copyright (c) 2024 FRC 2486
-// http://github.com/Coconuts2486-FRC
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// version 3 as published by the Free Software Foundation or
-// available in the root directory of this project.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+// // Copyright (c) 2024 FRC 6328
+// // http://github.com/Mechanical-Advantage
+// // Copyright (c) 2024 FRC 2486
+// // http://github.com/Coconuts2486-FRC
+// //
+// // This program is free software; you can redistribute it and/or
+// // modify it under the terms of the GNU General Public License
+// // version 3 as published by the Free Software Foundation or
+// // available in the root directory of this project.
+// //
+// // This program is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// // GNU General Public License for more details.
 
-package frc.robot;
+// package frc.robot;
 
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import lombok.Builder;
+// import edu.wpi.first.math.geometry.Transform2d;
+// import edu.wpi.first.wpilibj.DriverStation;
+// import lombok.Builder;
 
-/** Fudge factors for critical field locations which can be tuned per alliance at events. */
-public final class FudgeFactors {
-  @Builder
-  public static class FudgedTransform {
-    @Builder.Default private final Transform2d red = new Transform2d();
+// /** Fudge factors for critical field locations which can be tuned per alliance at events. */
+// public final class FudgeFactors {
+//   @Builder
+//   public static class FudgedTransform {
+//     @Builder.Default private final Transform2d red = new Transform2d();
 
-    @Builder.Default private final Transform2d blue = new Transform2d();
+//     @Builder.Default private final Transform2d blue = new Transform2d();
 
-    public Transform2d getTransform() {
-      return DriverStation.getAlliance()
-          .map(alliance -> alliance == DriverStation.Alliance.Red ? red : blue)
-          .orElseGet(Transform2d::new);
-    }
-  }
+//     public Transform2d getTransform() {
+//       return DriverStation.getAlliance()
+//           .map(alliance -> alliance == DriverStation.Alliance.Red ? red : blue)
+//           .orElseGet(Transform2d::new);
+//     }
+//   }
 
-  private FudgeFactors() {}
+//   private FudgeFactors() {}
 
-  public static final FudgedTransform speaker = FudgedTransform.builder().build();
+//   public static final FudgedTransform speaker = FudgedTransform.builder().build();
 
-  public static final FudgedTransform amp = FudgedTransform.builder().build();
+//   public static final FudgedTransform amp = FudgedTransform.builder().build();
 
-  public static final FudgedTransform centerPodiumAmpChain = FudgedTransform.builder().build();
+//   public static final FudgedTransform centerPodiumAmpChain = FudgedTransform.builder().build();
 
-  public static final FudgedTransform centerAmpSourceChain = FudgedTransform.builder().build();
+//   public static final FudgedTransform centerAmpSourceChain = FudgedTransform.builder().build();
 
-  public static final FudgedTransform centerSourcePodiumChain = FudgedTransform.builder().build();
-}
+//   public static final FudgedTransform centerSourcePodiumChain =
+// FudgedTransform.builder().build();
+// }
