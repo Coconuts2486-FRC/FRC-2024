bba4030639714c1af4b6be2d4c8d5fbbb4b18885
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-09-26 16:47:03 -0700

    Revert "Reapply "Mr. Bowers' small mucking""
    This reverts commit c7a7bd0ea3c7bf4835d466e224d346edab9cb04b.

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -322,7 +322,6 @@ public class Drive extends SubsystemBase {
    *
    * <p>NOTE: If we want the null result to return something other than -999.9, we can do that.
    */
-  @AutoLogOutput(key = "Targeting/SpeakerYaw")
   public static Rotation2d getSpeakerYaw() {
 
     // No tag information, return default value
@@ -341,7 +340,7 @@ public class Drive extends SubsystemBase {
       yaw = yaw.plus(new Rotation2d(Math.PI));
     }
 
-    // Return the YAW value as a Rotation2d object
+    // Return the YAW value in degrees
     return yaw;
   }
 
@@ -394,7 +393,6 @@ public class Drive extends SubsystemBase {
    *
    * <p>NOTE: If we want the null result to return something other than -999.9, we can do that.
    */
-  @AutoLogOutput(key = "Targeting/GamePieceYaw")
   public static Rotation2d getGamePieceYaw() {
 
     // No tag information, return default value
@@ -408,12 +406,12 @@ public class Drive extends SubsystemBase {
     // Add the game piece YAW to the current gyro value
     Rotation2d yaw = new Rotation2d(Units.degreesToRadians(DriveCommands.gyroYaw)).plus(gpYaw);
 
-    // Return the YAW value as a Rotation2d object
+    // Return the YAW value in degrees
     return yaw;
   }
 
-  @AutoLogOutput(key = "Targeting/GamePiecePose")
   public static Pose2d getGamePiecePose() {
+
     return GamePieceVision.gamePieceRelativePose;
   }
 }
